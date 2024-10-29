4c2b832fa6000b00c16405faa8b1f6755fead3f1
Takuyuki Kawakami <e225742@ie.u-ryukyu.ac.jp>
2024-10-29 14:17:01 +0900

    app/page変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-import { PrismaClient } from '@prisma/client';
-import { GetServerSideProps } from 'next';
 import React from 'react';
+import styles from './page.module.css';
+import Header from '@/components/header/header';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
@@ -10,37 +11,35 @@ type Question = {
   content: string;
 };
 
-type HomePageProps = {
-  questions: Question[];
-};
-
-const Home = ({ questions }: HomePageProps) => {
-  return (
-    <div>
-      <h1>最新の質問</h1>
-      <div>
-        {questions.map((question) => (
-          <div key={question.id}>
-            <h3>{question.title}</h3>
-            <p>{question.content}</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  // Prismaを使ってデータベースから最新の質問を取得
+async function fetchQuestions(): Promise<Question[]> {
   const questions = await prisma.question.findMany({
     orderBy: { createdAt: 'desc' },
   });
+  return questions;
+}
 
-  return {
-    props: {
-      questions,
-    },
-  };
-};
+export default async function Home() {
+  const questions = await fetchQuestions();
 
-export default Home;
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.introSection}>
+        <h2>相談広場へようこそ！</h2>
+      </div>
+
+      <main className={styles.main}>
+        <h2>最新の質問</h2>
+        <div className={styles.questionList}>
+          {questions.map((question) => (
+            <div key={question.id} className={styles.questionItem}>
+              <h3>{question.title}</h3>
+              <p>{question.content}</p>
+            </div>
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
